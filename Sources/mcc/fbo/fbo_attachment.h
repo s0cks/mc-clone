b2c10861ce6aefe2f496521d5998e95e5e868153
Tazz <tazz@tazz.cloud>
2024-04-24 05:19:10 -0400

    ....

--- Sources/mcc/fbo/fbo_attachment.h
+++ Sources/mcc/fbo/fbo_attachment.h
@@ -2,70 +2,28 @@
 #define MCC_FBO_ATTACHMENT_H
 
 #include <vector>
-#include "mcc/texture/texture.h"
+#include "mcc/fbo/fbo_target.h"
 
 namespace mcc::fbo {
 #define FOR_EACH_FBO_ATTACHMENT_TYPE(V) \
-  V(ColorBuffer)                                \
-  V(RenderBuffer)                               \
-  V(Picking)
+  V(Color)                              \
+  V(Depth)                              \
+  V(Stencil)
 
-#define FORWARD_DECLARE(Name) class Name##Attachment;
+  class FboAttachment;
+#define FORWARD_DECLARE(Name) class Fbo##Name##Attachment;
   FOR_EACH_FBO_ATTACHMENT_TYPE(FORWARD_DECLARE)
 #undef FORWARD_DECLARE
 
-  enum FrameBufferAttachmentType {
-#define DEFINE_TYPE(Name) k##Name##Attachment,
-    FOR_EACH_FBO_ATTACHMENT_TYPE(DEFINE_TYPE)
-#undef DEFINE_TYPE
-    kNumberOfFrameBufferAttachmentTypes,
-  };
-
-  typedef GLuint FrameBufferAttachmentTarget;
-
-  class FrameBufferAttachment {
+  class FboAttachment {
   protected:
-    bool enabled_;
-    FrameBufferAttachmentTarget target_;
+    FboAttachment() = default;
 
-    FrameBufferAttachment(const bool enabled, const FrameBufferAttachmentTarget target):
-      enabled_(enabled),
-      target_(target) {
-    }
-    explicit FrameBufferAttachment(const FrameBufferAttachmentTarget target):
-      FrameBufferAttachment(true, target) {
-    }
+    virtual void AttachTo(const FboTarget target) = 0;
   public:
-    virtual ~FrameBufferAttachment() = default;
-    virtual const char* name() const = 0;
-    virtual FrameBufferAttachmentType type() const = 0;
-
-    virtual bool IsEnabled() const {
-      return enabled_;
-    }
-
-    FrameBufferAttachmentTarget target() const {
-      return target_;
-    }
-
-#define DEFINE_TYPE_CAST(Name) \
-    virtual Name##Attachment* As##Name##Attachment() { return nullptr; }
-    FOR_EACH_FBO_ATTACHMENT_TYPE(DEFINE_TYPE_CAST)
-#undef DEFINE_TYPE_CAST
-
-#define DEFINE_TYPE_CHECK(Name) \
-    inline bool Is##Name##Attachment() const { return type() == k##Name##Attachment; }
-    FOR_EACH_FBO_ATTACHMENT_TYPE(DEFINE_TYPE_CHECK)
-#undef DEFINE_TYPE_CHECK
+    virtual ~FboAttachment() = default;
+    virtual std::string ToString() const = 0;
   };
-
-  typedef std::vector<FrameBufferAttachment*> FrameBufferAttachmentList;
-
-#define DEFINE_FBO_ATTACHMENT(Name)                                                                     \
-  public:                                                                                                       \
-    const char* name() const override { return #Name; }                                                         \
-    FrameBufferAttachmentType type() const override { return FrameBufferAttachmentType::k##Name##Attachment; }  \
-    Name##Attachment* As##Name##Attachment() override { return this; }
 }
 
 #include "mcc/fbo/fbo_attachment_color.h"
