d22bb29a301bcf25d37a1ff32c4c51c1ff2b1176
Tazz <tazz@tazz.cloud>
2024-05-08 02:18:51 -0400

    ...

--- Benchmarks/main.cc
+++ Benchmarks/main.cc
@@ -1,12 +1,12 @@
 #include <glog/logging.h>
-#include <gflags/gflags.h>
 #include <benchmark/benchmark.h>
+#include "mcc/flags.h"
 
 int main(int argc, char** argv) {
+  ::google::ParseCommandLineFlags(&argc, &argv, true);
   ::google::InitGoogleLogging(argv[0]);
   ::google::LogToStderr();
   ::benchmark::Initialize(&argc, argv);
-  ::google::ParseCommandLineFlags(&argc, &argv, false);
   ::benchmark::RunSpecifiedBenchmarks();
   ::benchmark::Shutdown();
   return EXIT_SUCCESS;
