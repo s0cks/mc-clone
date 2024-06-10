a074b1a9f0a213d4a30e21c05b4a865498161ec7
Tazz <tazz@tazz.cloud>
2024-06-10 15:31:20 -0400

    add WindowBuilder

--- Sources/mcc/window/window_builder.cc
+++ Sources/mcc/window/window_builder.cc
@@ -1,5 +1,21 @@
 #include "mcc/window/window_builder.h"
 
 namespace mcc::window {
-  
+  Window* WindowBuilder::Build() const {
+    NOT_IMPLEMENTED(FATAL);
+    return nullptr;
+  }
+
+  rx::observable<Window*> WindowBuilder::BuildAsync() const {
+    return rx::observable<>::create<Window*>([this](rx::subscriber<Window*> s) {
+      const auto value = Build();
+      if(!value) {
+        const auto err = "failed to build Window.";
+        return s.on_error(rx::util::make_error_ptr(std::runtime_error(err)));
+      }
+
+      s.on_next(value);
+      s.on_completed();
+    });
+  }
 }
--- Sources/mcc/window/window_builder.h
+++ Sources/mcc/window/window_builder.h
@@ -1,12 +1,65 @@
 #ifndef MCC_WINDOW_BUILDER_H
 #define MCC_WINDOW_BUILDER_H
 
+#include <string>
+#include "mcc/rx.h"
+#include "mcc/builder.h"
+#include "mcc/window/window_constants.h"
+
 namespace mcc::window {
   class Window;
-  class WindowBuilder {
+  class WindowBuilder : public BuilderTemplate<Window> {
+  private:
+    bool fullscreen_;
+    bool resizable_;
+    std::string title_;
+    glm::u32vec2 size_;
   public:
     WindowBuilder() = default;
     virtual ~WindowBuilder() = default;
+
+    void SetFullscreen(const bool value = true) {
+      fullscreen_ = value;
+    }
+
+    inline void ClearFullscreen() {
+      return SetFullscreen(false);
+    }
+
+    bool IsFullscreen() const {
+      return fullscreen_;
+    }
+
+    void SetResizable(const bool value = true) {
+      resizable_ = value;
+    }
+
+    inline void ClearResizable() {
+      return SetResizable(false);
+    }
+
+    bool IsResizable() const {
+      return resizable_;
+    }
+
+    void SetTitle(const std::string& value) {
+      title_ = value;
+    }
+
+    const std::string& GetTitle() const {
+      return title_;
+    }
+
+    void SetSize(const glm::u32vec2& size) {
+      size_ = size;
+    }
+
+    const glm::u32vec2& GetSize() const {
+      return size_;
+    }
+
+    Window* Build() const override;
+    rx::observable<Window*> BuildAsync() const override;
   };
 }
 
